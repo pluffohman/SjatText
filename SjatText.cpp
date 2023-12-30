fd454062082a7ec5a3c9b485c5c710bce8282b8e
matvey <matveykakiselev7@gmail.com>
2023-12-30 18:20:20 +0300

    Добавьте файлы проекта.

---
+++ SjatText.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+string sjat(string input) {
+    string news;
+
+    bool isprobel = true;
+
+    for (auto& i : input) {
+        if (i == ' ') {
+            isprobel = true;
+        }
+        else {
+            if (isprobel) {
+                news += ' ';
+            }
+            news += i;
+            isprobel = false;
+        }
+    }
+
+    return news;
+}
+
+int main() {
+    string s;
+    getline(cin, s);
+    string compressedText = sjat(s);
+    cout << compressedText << endl;
+
+}
